752e80137ec49d104cfdf8eb2fd689551b378339
Rebecca Liu <yliu1033@bloomberg.net>
2018-12-14 00:03:10 -0800

    final

--- Project/training_graident_boosting.py
+++ Project/training_graident_boosting.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn import ensemble
+import xgboost
+from sklearn import ensemble, tree
 from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split, cross_val_score, KFold
+from sklearn.model_selection import train_test_split, cross_val_score, KFold, cross_validate
 from sklearn import metrics
 # models
 
@@ -13,7 +14,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 PCT_CHANGE_THRESHOLD = 0.02
 
 
-def train_gridient_boost(X, y, clf, y_type, plot_graph, cv):
+def train(X, y, clf, y_type, plot_graph, cv):
     X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2)
     clf.fit(X_train, y_train)
     if cv:
@@ -24,6 +25,7 @@ def train_gridient_boost(X, y, clf, y_type, plot_graph, cv):
         for mean, std, params in zip(means, stds, clf.cv_results_['params']):
             print("%0.3f (+/-%0.03f) for %r"
                   % (mean, std * 2, params))
+
 
     predict_y = clf.predict(X_valid)
     mse = mean_squared_error(y_valid, predict_y)
@@ -39,7 +41,7 @@ def train_gridient_boost(X, y, clf, y_type, plot_graph, cv):
     # Plot training deviance
     if plot_graph:
 
-        # compute test set deviance
+        #compute test set deviance
         test_score = np.zeros((clf.n_estimators,), dtype=np.float64)
 
         for i, y_pred in enumerate(clf.staged_predict(X_valid)):
@@ -48,11 +50,12 @@ def train_gridient_boost(X, y, clf, y_type, plot_graph, cv):
         plt.figure(figsize=(12, 6))
         plt.subplot(1, 2, 1)
         plt.title('Score ' + y_type)
-        plt.plot(np.arange(clf.n_estimators) + 1, clf.train_score_, 'b-',
-                 label='Training Set Score')
-        plt.plot(np.arange(clf.n_estimators) + 1, test_score, 'r-',
-                 label='Test Set Score')
-        plt.legend(loc='upper right')
+        # # since for gradient boosting, the loss function is loss='deviance the score is deviance result'
+        # plt.plot(np.arange(clf.n_estimators) + 1, clf.train_score_, 'b-',
+        #          label='Training Set Score')
+        # plt.plot(np.arange(clf.n_estimators) + 1, test_score, 'r-',
+        #          label='Test Set Score')
+        # plt.legend(loc='upper right')
         plt.xlabel('Boosting Iterations')
         plt.ylabel('Score')
 
@@ -63,7 +66,7 @@ def train_gridient_boost(X, y, clf, y_type, plot_graph, cv):
         feature_importance = 100.0 * (feature_importance / feature_importance.max())
         sorted_idx = np.argsort(feature_importance)
         pos = np.arange(sorted_idx.shape[0]) + .5
-        plt.subplot(1, 2, 2)
+        plt.subplot(1, 1, 1)
         plt.barh(pos, feature_importance[sorted_idx], align='center')
         plt.yticks(pos, X.columns[sorted_idx])
         plt.xlabel('Relative Importance')
@@ -99,28 +102,37 @@ if __name__ == "__main__":
 
         #normal test case
         gradient_boosting = ensemble.GradientBoostingClassifier(**params)
-        train_gridient_boost(X, y, gradient_boosting, y_name, True, False)
+        train(X, y, gradient_boosting, y_name, True, False)
 
-
-        #10 fold
+        print("-- Decision Tree Bagging (Generalization of Random Forest)")
+        bagging_clf = ensemble.BaggingClassifier(tree.DecisionTreeClassifier(max_depth=8, min_samples_split=12),
+                                                 n_estimators=100, max_samples=0.9, max_features=0.9)
+        #
+        # #10 fold
         kfold = KFold(n_splits=10, shuffle=True)
-        results = cross_val_score(gradient_boosting, X, y, cv=kfold)
-        print("10-fold cross_val score: %.2f%% (%.2f%%)" % (results.mean() * 100, results.std() * 100))
+        results = cross_validate(bagging_clf, X, y, cv=kfold,return_train_score=True)
+        print("train score: %.2f%% (%.2f%%)" % (results['train_score'].mean() * 100, results['train_score'].std() * 100))
+        print("10-fold cross_val score: %.2f%% (%.2f%%)" % (results['test_score'].mean() * 100, results['test_score'].std() * 100))
+        print("fit time: %.2f score time:%.2f" % (results['fit_time'].mean(),results['score_time'].mean()))
+
+        bagging_clf = ensemble.BaggingClassifier(tree.DecisionTreeClassifier(max_depth=8, min_samples_split=12),
+                                                 n_estimators=100, max_samples=0.9, max_features=0.9)
+        xgboost_params = {'n_estimators': 150, 'max_depth': 6, 'gamma': 10, 'learning_rate': 0.1}
+        xgboost_clf = xgboost.XGBClassifier(**xgboost_params)
+        ertrees_clf = ensemble.ExtraTreesClassifier(n_estimators=1000, max_depth=12, min_samples_split=16,
+                                                    max_features=0.5)
+
+        # -- Hard Voting  with bagging, xgboost, extremely randomized trees
+        hard_voting_clf = ensemble.VotingClassifier(estimators=[('bagging', bagging_clf),
+                                                                ('xgboost', xgboost_clf), ('ertrees', ertrees_clf)],
+                                                    voting='hard')
+        train(X, y, xgboost_clf, y_name, True, False)
 
 
 
         #test parameter
-        param_test1 = {'min_samples_split':range(20,100,10),'min_samples_leaf':range(1,5,1)}
-        gsearch1 = GridSearchCV(estimator=ensemble.GradientBoostingClassifier(**params),param_grid=param_test1, cv=10)
-        train_gridient_boost(X, y, gsearch1, y_name, False,True)
-        print()
+        # param_test1 = {'n_estimators':range(100,1000,100)}
+        # gsearch1 = GridSearchCV(ensemble.BaggingClassifier(tree.DecisionTreeClassifier(max_depth = 8, min_samples_split = 12), n_estimators = 600, max_samples = 0.9, max_features = 0.9),param_grid=param_test1, cv=5)
+        # train_gridient_boost(X, y, gsearch1, y_name, False,True)
+        # print()
 
-    # y = train_df['sustained_average_1_to_130_days_later']
-    # print("-- gradient boosting")
-    # print("sustained_average_1_to_130_days_later")
-    # print("baseline is {}".format(sum(y == 1) / len(y)))
-    # params = {'n_estimators': 500, 'max_depth': 4, 'min_samples_split': 2, 'learning_rate': 0.01}
-    # gradient_boosting = ensemble.GradientBoostingClassifier(**params)
-    # print(gradient_boosting)
-    # train_gridient_boost(X, y, gradient_boosting,"ustained_average_1_to_130_days_later")
-    # print()
